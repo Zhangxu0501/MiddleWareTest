ce5308e6fd2b3e721604860d48fc37252d203528
Zhangxu0501 <81248397@qq.com>
2017-04-06 18:02:30 +0800

    up

--- src/test2_NIO/Test.java
+++ src/test2_NIO/Test.java
@@ -1,15 +1,33 @@
 package test2_NIO;
 
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
 public class Test
 {
 	public static void sop(Object o)
 	{
 		System.out.println(o);
 	}
-	public static void main(String[] args)
+	public static void main(String[] args) throws Exception
 	{
-		// TODO Auto-generated method stub
-
+		ByteBuffer bf =ByteBuffer.allocate(1024);
+		RandomAccessFile raf = new RandomAccessFile("/Users/zhangxu/Desktop/未命名文件夹/自传","rw");
+		FileChannel fc = raf.getChannel();
+		int len = fc.read(bf);
+		while(len!=-1)
+		{
+			bf.flip();
+			while(bf.hasRemaining())
+				sop(bf.get());
+			byte bs = bf.get();
+			
+			//sop(bs.length);
+			bf.clear();
+			len=fc.read(bf);
+		}
+		
 	}
 
 }
